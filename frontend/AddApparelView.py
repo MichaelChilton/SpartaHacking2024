0f1f73bf04873f954ce45919d152e56686087672
Jared Dunham <jaredscottdunham@gmail.com>
2024-01-27 15:59:49 -0500

    Apparel Update

---
+++ frontend/AddApparelView.py
@@ -0,0 +1,44 @@
+import os
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import SlideTransition
+
+class AddApparelView(Screen):
+    def __init__(self, **kwargs):
+        super(AddApparelView, self).__init__(**kwargs)
+        # create a dropdown with 10 buttons
+
+        dropdown = DropDown()
+        for index in range(10):
+            # When adding widgets, we need to specify the height manually
+            # (disabling the size_hint_y) so the dropdown can calculate
+            # the area it needs.
+
+            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
+
+            # for each button, attach a callback that will call the select() method
+            # on the dropdown. We'll pass the text of the button as the data of the
+            # selection.
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+
+            # then add the button inside the dropdown
+            dropdown.add_widget(btn)
+
+        # create a big main button
+        mainbutton = Button(text='Hello', size_hint=(None, None))
+
+        # show the dropdown menu when the main button is released
+        # note: all the bind() calls pass the instance of the caller (here, the
+        # mainbutton instance) as the first argument of the callback (here,
+        # dropdown.open.).
+        mainbutton.bind(on_release=dropdown.open)
+
+        # one last thing, listen for the selection in the dropdown list and
+        # assign the data to the button text.
+        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from frontend.MyClosetView import MyClosetView
+from frontend.AddApparelView import AddApparelView
 # to add new screen, import it here like the MyClosetView above
 
 
 class MainApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(MyClosetView(name='my_closet'))
+        sm.add_widget(AddApparelView(name='Apparel'))
         # to add new screen, add it here like the MyClosetView above
         return sm
 
