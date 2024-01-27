1cebcf71ec8d4210cd56abc4e3a3b0c5cf834891
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2024-01-27 18:59:53 -0500

    its time

--- backend/camView.py
+++ backend/camView.py
@@ -1,28 +1,28 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.camera import Camera
 
-
-class CameraApp(App):
-    def build(self):
-        self.layout = FloatLayout()
+class camView(Screen):
+    def __init__(self, screen_manager, **kwargs):
+        super(camView, self).__init__(**kwargs)
+        self.screen_manager = screen_manager
+        self.layout = BoxLayout(orientation='vertical')
 
         # Create a camera object
         self.camera = Camera(play=True, resolution=(640, 480))
         self.layout.add_widget(self.camera)
 
         # Create a button to capture the image
-        self.capture_button = Button(text='Capture', size_hint=(None, None), size=(100, 50), pos_hint={'center_x': 0.5, 'y': 0})
+        self.capture_button = Button(text='Capture', size_hint=(None, None), size=(100, 50))
         self.capture_button.bind(on_press=self.capture)
         self.layout.add_widget(self.capture_button)
+        self.add_widget(self.layout)
 
-        return self.layout
 
     def capture(self, instance):
         # Capture the image from the camera
         self.camera.export_to_png('cool_guy_kivy.png')  # Save the image as 'cool_guy_kivy.png'
 
-
-if __name__ == '__main__':
-    CameraApp().run()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from frontend.MyClosetView import MyClosetView
-from backend.cameraView import cameraView
+from backend.camView import camView
 # to add new screen, import it here like the MyClosetView above
 from frontend.MyOutfit import SecondView
 
@@ -10,7 +10,7 @@ class MainApp(App):
     def build(self):
         sm = ScreenManager()
 
-        sm.add_widget(cameraView(name='cameraView'))
+        sm.add_widget(camView(screen_manager=sm, name='cameraView'))
         
         # to add new screen, add it here like the MyClosetView above
         return sm
