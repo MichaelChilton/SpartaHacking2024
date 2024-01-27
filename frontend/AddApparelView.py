a776b9e93f235b08dcc7be76bb2e3ff6c220e4bb
Jared Dunham <jaredscottdunham@gmail.com>
2024-01-27 17:50:03 -0500

    Update AddApparelView.py

--- frontend/AddApparelView.py
+++ frontend/AddApparelView.py
@@ -4,6 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
+from kivy.base import runTouchApp
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import SlideTransition
@@ -15,12 +16,16 @@ class AddApparelView(Screen):
         # create a dropdown with 10 buttons
 
         dropdown = DropDown()
-        for index in range(10):
+
+        # List of apparel types
+        apparel_types = ['Shirt', 'Pants', 'Shoes']
+
+        for apparel_type in apparel_types:
             # When adding widgets, we need to specify the height manually
             # (disabling the size_hint_y) so the dropdown can calculate
             # the area it needs.
 
-            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
+            btn = Button(text=apparel_type, size_hint_y=None, height=40)
 
             # for each button, attach a callback that will call the select() method
             # on the dropdown. We'll pass the text of the button as the data of the
@@ -31,7 +36,7 @@ class AddApparelView(Screen):
             dropdown.add_widget(btn)
 
         # create a big main button
-        mainbutton = Button(text='Hello', size_hint=(None, None))
+        mainbutton = Button(text='Apparel Type', size_hint=(None, None))
 
         # show the dropdown menu when the main button is released
         # note: all the bind() calls pass the instance of the caller (here, the
@@ -42,5 +47,7 @@ class AddApparelView(Screen):
         # one last thing, listen for the selection in the dropdown list and
         # assign the data to the button text.
         dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+        runTouchApp(mainbutton)
 
         self.add_widget(dropdown)
