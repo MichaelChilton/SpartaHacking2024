f6a7d91709360e37f01fe6120960c6c9a119d212
Jared Dunham <jaredscottdunham@gmail.com>
2024-01-28 07:04:04 -0500

    Create Outfit

---
+++ backend/Outfit.py
@@ -0,0 +1,25 @@
+class Outfit:
+    def __init__(self):
+        self.top = None
+        self.pants = None
+        self.shoes = None
+
+    def get_top(self):
+        return self.top
+
+    def set_top(self, top):
+        self.top = top
+
+    def get_pants(self):
+        return self.pants
+    
+    def set_pants(self, pants):
+        self.pants = pants
+
+    def get_shoes(self):
+        return self.shoes
+
+    def set_shoes(self, shoes):
+        self.category = shoes
+    
+
